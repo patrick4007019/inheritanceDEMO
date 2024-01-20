273b6659df5726f060bd0cfd45fc0b356b8f5f11
patrick4007019 <patrick.4007019@gmail.com>
2024-01-19 19:18:36 -0500

    Changed myShape to Triangle

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Shape equilateral = new Circle(10);
+        Shape equilateral = new Triangle(5, 10, 10, 10, 10);
         System.out.println(equilateral.calculateArea());
         System.out.println(equilateral.calculatePerimeter());
     }
